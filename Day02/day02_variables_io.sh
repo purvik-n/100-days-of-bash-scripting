a57b92bbc82371e14b59e6a5fa949abc9c4110ea
Purvik N Gowda <purviknarasimha20@gmail.com>
2025-08-19 13:21:15 -0400

    Create day02_variables_io.sh

---
+++ Day02/day02_variables_io.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name="Purvik N Gowda"
+age=20
+city="New York"
+echo "Name: $name"
+echo "Age: $age"
+echo "City: $city"
+echo "Hello ${name}, you are ${age} years old"
+echo "Current user: $USER"
+echo "Home directory: $HOME"
+echo "Current shell: $SHELL"
